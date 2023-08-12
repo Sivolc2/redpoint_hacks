4cc271980283e6bf3e12b11d97776509c0e27949
Sandy Tanwisuth <11690577+sandguine@users.noreply.github.com>
2023-08-12 16:38:14 -0700

    add replicate call directly on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 import os
+import replicate
+import webbrowser
 
 import streamlit as st
 
 DB_PATH = (Path(__file__).parent / "Chinook.db").absolute()
+
+# Replicate Variables
+model = replicate.models.get("stability-ai/stable-diffusion")
+version = model.versions.get(
+    "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
 
 
 # st.title("🦜 LangChain: Chat with search")
@@ -22,6 +29,8 @@ tabs = st.tabs(["Visualization"])
 with tabs[0]:
     with st.form(key="form"):
         user_input = st.text_input("Add an item to your visualizer")
+        output_url = version.predict(
+            prompt = user_input)[0]
         # if not user_input:
         #     user_input = prefilled
         submit_clicked = st.form_submit_button("Build visualization")
