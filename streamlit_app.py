d7275b5a0c39b546e0500984dfff89ba00906aaf
sivolc <sivolc11@gmail.com>
2023-08-12 18:56:28 -0700

    v0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import replicate
+
 import streamlit as st
 from streamlit.components.v1 import html
 
@@ -14,72 +15,75 @@ def build_prompt(user_input):
     # Placeholder. Modify as required.
     return user_input
 
-def export_prompt(processed_prompt):
-    with open('./data/output_prompt.txt', 'a') as file:
-        file.write(processed_prompt + '\n')
+# def export_prompt(processed_prompt):
+#     with open('./data/output_prompt.txt', 'a') as file:
+#         file.write(processed_prompt + '\n')
 
+# st.title("🦜 LangChain: Chat with search")
 st.set_page_config(
     page_title="Theia", page_icon="*", layout="wide", initial_sidebar_state="collapsed"
 )
 
 "# 🦜🔗 Theia: Connect your ideas"
-
-# Ensure you have valid API keys.
+openai_api_key = st.write(
+    os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"],
+)
+# Replicate Variables
+replicate_api_key = st.write(
+    os.environ["REPLICATE_API_TOKEN"] == st.secrets["REPLICATE_API_TOKEN"],
+)
 model = replicate.models.get(MODEL)
 version = model.versions.get(VERSION)
 
+# Existing imports and setup
 tabs = st.tabs(["Input", "Visualization"])
 
 with tabs[0]:
     with st.form(key="form"):
         user_input = st.text_input("Add an item to your visualizer")
+        processed_prompt = build_prompt(user_input)
+        output_url = version.predict(
+            prompt = processed_prompt)[0]
+        
+        # Display the image in the Streamlit app
+        st.image(output_url, caption='Generated Image', use_column_width=True)
+
         submit_clicked = st.form_submit_button("Build visualization")
 
-        if user_input:
-            processed_prompt = build_prompt(user_input)
-            export_prompt(processed_prompt)
-            
-            # Now read from the saved prompts and generate the graph
-            with open('./data/output_prompt.txt', 'r') as file:
-                lines = file.readlines()
+        my_html = """
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <!-- You might want to include meta and other tags here. -->
+            </head>
+            <body>
 
-            graph_nodes = []
-            graph_relations = []
-            for idx, line in enumerate(lines):
-                line = line.strip()
-                output_url = version.predict(prompt=line)[0]
-                node_str = f"A{idx}[<img src='{output_url}' width='100' height='100' />]"
-                graph_nodes.append(node_str)
+            <div id="app"></div> <!-- This is where the Mermaid graph will render. -->
 
-                if idx > 0:
-                    graph_relations.append(f"A{idx-1}-->A{idx}")
-            
-            graph_definition = ";\n".join(graph_nodes + graph_relations)
-            # Construct the graph in HTML
-            graph_def_script = f"""var graphDefinition = `{graph_definition}`;var graph = mermaidAPI.render("mermaid", graphDefinition, insertSvg);
-                </script>
-                </body>
-                </html>"""
-            
-            my_html = f"""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <!-- You might want to include meta and other tags here. -->
-                </head>
-                <body>
+            <script src="https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.min.js"></script>
 
-                <div id="app"></div> <!-- This is where the Mermaid graph will render. -->
+            <!-- Inlined JavaScript from mermaid_graph.js -->
+            <script type="text/javascript">
+                var mermaidAPI = mermaid.mermaidAPI;
 
-                <script src="https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.min.js"></script>
-                <script type="text/javascript">
-                    var mermaidAPI = mermaid.mermaidAPI;
-                    mermaidAPI.initialize({{ startOnLoad: false }});
-                    var element = document.getElementById("app");
-                    var insertSvg = function(svgCode, bindFunctions) {{
-                        element.innerHTML = svgCode;
-                    }};                    
-            """ + graph_def_script
+                mermaidAPI.initialize({
+                startOnLoad: false
+                });
 
-            print('Running', my_html)
-            html(my_html)
+                var element = document.getElementById("app");
+                var insertSvg = function(svgCode, bindFunctions) {
+                element.innerHTML = svgCode;
+                };
+        """ + f"""
+    
+                var graphDefinition = `graph LR; Nodetext-->SomeIcon(<img src='{output_url}' width='100' height='100' />)`;
+                var graphDefinition2; // This variable is declared but not used in your provided script.
+                var graph = mermaidAPI.render("mermaid", graphDefinition, insertSvg);
+            </script>
+
+            </body>
+            </html>
+        """
+
+with tabs[1]:
+    html(my_html)
