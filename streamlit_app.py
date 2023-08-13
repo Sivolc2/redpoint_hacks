27098b9dbc5038493aeeddfab0bb2e76be1bfba7
sivolc <sivolc11@gmail.com>
2023-08-12 17:02:10 -0700

    testing js example

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 
 import streamlit as st
+from streamlit.components.v1 import html
 
 DB_PATH = (Path(__file__).parent / "Chinook.db").absolute()
 
@@ -26,4 +27,19 @@ with tabs[0]:
         # if not user_input:
         #     user_input = prefilled
         submit_clicked = st.form_submit_button("Build visualization")
-# jupyter nbconvert --to script *.ipynb
+
+
+# import streamlit as st
+
+# Define your javascript
+my_js = """
+alert("Hola mundo");
+"""
+
+# Wrapt the javascript as html code
+my_html = f"<script>{my_js}</script>"
+
+# Execute your app
+st.title("Javascript example")
+html(my_html)
+
